6aaecd2d2a2d001a0a9a3b9d61a7058abc2c3a71
Muhammad Rifat Fadhillah <muhammad.rifat11@ui.ac.id>
2022-09-15 15:20:14 +0700

    Lab 02

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_xml
+from wishlist.views import show_json
+from wishlist.views import show_json_by_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
+
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
@@ -7,3 +10,13 @@ def show_wishlist(request):
         'nama': 'Muhammad Rifat Fadhillah'
 }
     return render(request, "wishlist.html", context)
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+def show_json_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    
